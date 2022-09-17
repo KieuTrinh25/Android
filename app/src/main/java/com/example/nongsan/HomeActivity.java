6e3d72db9e6296e8dfb8ec3b95b09e8356310ce0
trinh <trinh0792545710@gmail.com>
2022-09-17 14:54:10 +0700

    update

--- app/src/main/java/com/example/nongsan/HomeActivity.java
+++ app/src/main/java/com/example/nongsan/HomeActivity.java
@@ -13,33 +13,35 @@ import com.example.nongsan.ui.constract.HomePresenter;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements HomeConstract.IView {
+public class HomeActivity extends AppCompatActivity implements HomeConstract.IView {
     private HomeConstract.IPresenter mPresenter;
-    RecyclerView rc;
+    private  RecyclerView rc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
+
+        initGUI();
+        initData();
     }
     private void initGUI(){
         rc = findViewById(R.id.rc);
         rc.setLayoutManager(new LinearLayoutManager(this));
 
-
-
     }
 
     private void initData(){
         mPresenter = new HomePresenter(this);
-        mPresenter.getCategoryList();
         mPresenter.setView(this);
+        mPresenter.getCategoryList();
     }
 
     @Override
     public void setCategoryListToView(List<Category> categoryList) {
         CategoryAdapter adapter = new CategoryAdapter(categoryList);
         rc.setAdapter(adapter);
+        rc.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
     }
 
     @Override
--- app/src/main/java/com/example/nongsan/utils/Constants.java
+++ app/src/main/java/com/example/nongsan/utils/Constants.java
@@ -1,7 +1,7 @@
 package com.example.nongsan.utils;
 
 public class Constants {
-    public static final String DATABASE_PATH = "/data/data/com.example.nongsan/databases";
+    public static final String DATABASE_PATH = "/data/data/com.example.nongsan/databases/";
     public static final String DATABASE_NAME = "shop.sqlite";
     public static final int DATABASE_VERSION = 1;
 }
